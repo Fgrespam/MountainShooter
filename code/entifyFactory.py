6324ed174dddf1ab03c1dd2cbc7cd8f5dbe9b50b
fgrespamads@hotmail.com <fgrespamads@hotmail.com>
2025-08-04 21:22:36 -0300

    adding classes generated from StarUML

---
+++ code/entifyFactory.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class EntifyFactory:
+    def __init__(self):
+        pass
+
+    def get_entify(self, entity_type):
+        pass
--- main.py
+++ main.py
@@ -10,6 +10,5 @@ while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print('teste...')
             pygame.quit()  # Close Window
             exit()  # end pygame
